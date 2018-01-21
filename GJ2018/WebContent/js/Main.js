a05ef1afd778d78ede8ab07d55b2a4e7ea2caf7e
Edouard Shaar <Zarthia@gmail.com>
2018-01-20 19:21:07 -0500

    eslint working, game broken. Use out class and not the phaser one in the index.html file...

--- GJ2018/WebContent/js/Main.js
+++ GJ2018/WebContent/js/Main.js
@@ -1,22 +1,22 @@
-import 'pixi';
-import 'p2';
 import Phaser from 'phaser';
 
 import Level from './assets/canvas/Level';
 
-class Game extends Phaser.Game {
+export default class Game extends Phaser.Game {
+  constructor() {
+    super(800, 600, Phaser.AUTO, '', null, false, true, {
+      arcade: true,
+      matter: true,
+      p2: true,
+      ninja: true
+    });
 
-    constructor() {
-        super(800, 600, Phaser.AUTO, '', null, false, true, { "arcade": true, "matter": true, "p2": true, "ninja": true });
+    // Add the States your game has.
+    // this.state.add("Boot", Boot);
+    // this.state.add("Menu", Menu);
+    // this.state.add("Preload", Preload);
+    this.state.add('Level', Level);
 
-        // Add the States your game has.
-        // this.state.add("Boot", Boot);
-        // this.state.add("Menu", Menu);
-        // this.state.add("Preload", Preload);
-        this.state.add("Level", Level);
-
-        this.state.start('Level');
-    }
+    this.state.start('Level');
+  }
 }
-
-window.game = new Game();
--- GJ2018/WebContent/js/assets/canvas/Level.js
+++ GJ2018/WebContent/js/assets/canvas/Level.js
@@ -1,18 +1,16 @@
+import Phaser from 'phaser';
+
 class Level extends Phaser.State {
-	init() {
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.pageAlignHorizontally = true;
-		this.scale.pageAlignVertically = true;
-		this.stage.backgroundColor = '#ffffff';
-	}
+  init() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+    this.stage.backgroundColor = '#ffffff';
+  }
 
-	preload() {
-
-	}
-
-	create() {
-		this.add.text(344, 292, 'Hello world!', { font: 'bold 20px Arial' });
-	}
+  create() {
+    this.add.text(344, 292, 'Hello world!', { font: 'bold 20px Arial' });
+  }
 }
 
 export default Level;
