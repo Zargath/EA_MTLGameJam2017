20be3550b676a0534e1a2cd72d5952efb9ff732f
Edouard Shaar <Zarthia@gmail.com>
2018-01-26 23:51:00 -0500

    Rooms now connect and works in general now.

--- js/Map/Map.js
+++ js/Map/Map.js
@@ -1,4 +1,4 @@
-import { random } from 'lodash';
+import { random, last } from 'lodash';
 
 import Room from './Room';
 
@@ -7,11 +7,12 @@ const Wall = 'W';
 
 export default class Map {
   constructor(width, height, maxRoomCount) {
-    this.width = width;
-    this.height = height;
     this.maxRoomCount = maxRoomCount;
     this.minRoomSize = 3;
-    this.maxRoomSize = 6;
+    this.maxRoomSize = 5;
+
+    this.width = width + this.maxRoomSize;
+    this.height = height + this.maxRoomSize;
     this.rooms = [];
     this.map = [];
 
@@ -29,27 +30,60 @@ export default class Map {
   }
 
   placeRooms() {
-    for (let roomCount = 0; roomCount < this.maxRoomCount; roomCount++) {
+    let tries = 0;
+    while (this.rooms.length < this.maxRoomCount) {
       const roomWidth = this.minRoomSize + random((this.maxRoomSize - this.minRoomSize) + 1);
       const roomHeight = this.minRoomSize + random((this.maxRoomSize - this.minRoomSize) + 1);
 
-      const xLoc = random(this.width - roomWidth - 1) + 1;
-      const yLoc = random(this.height - roomHeight - 1) + 1;
+      const xLoc = random(this.width - this.maxRoomSize - roomWidth - 1) + 1;
+      const yLoc = random(this.height - this.maxRoomSize - roomHeight - 1) + 1;
 
       const newRoom = new Room(xLoc, yLoc, roomHeight, roomWidth);
 
       let failed = false;
       this.rooms.forEach((r) => {
-        if (!newRoom.intersects(r) && newRoom.doesRoomFitOnMap(this.map)) {
+        if (newRoom.intersects(r)) {
           failed = true;
         }
       });
 
       if (!failed) {
         this.addRoomToMap(newRoom);
+        this.connectRooms(newRoom, last(this.rooms));
         this.rooms.push(newRoom);
+      } else {
+        tries++;
+      }
+
+      // Hack to avoid infinite loop ;)
+      if (tries >= 20) {
+        break;
       }
     }
+  }
+
+  connectRooms(room1, room2) {
+    if (room2 === null || room2 === undefined) {
+      return;
+    }
+
+    const statingX = Math.min(room1.centerCoordinate.x, room2.centerCoordinate.x);
+    const endingX = Math.max(room1.centerCoordinate.x, room2.centerCoordinate.x);
+
+    for (let x = statingX; x < endingX; x++) {
+      this.addFloorToMap(x, room1.centerCoordinate.y);
+    }
+
+    const statingY = Math.min(room1.centerCoordinate.y, room2.centerCoordinate.y);
+    const endingY = Math.max(room1.centerCoordinate.y, room2.centerCoordinate.y);
+
+    for (let y = statingY; y < endingY; y++) {
+      this.addFloorToMap(room2.centerCoordinate.x, y);
+    }
+  }
+
+  addFloorToMap(x, y) {
+    this.addToMap(x, y, 1, 1, Floor);
   }
 
   addRoomToMap(room) {
@@ -65,6 +99,7 @@ export default class Map {
   }
 
   debug() {
-    console.log(this.map);
+    console.log(`Room Count: ${this.rooms.length}`);
+    console.table(this.map);
   }
 }
