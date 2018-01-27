64ca17c14d9454812f23c2d4c93931deca61b70a
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2018-01-27 18:04:13 -0500

    Created the help menu

--- js/Hud.js
+++ js/Hud.js
@@ -1,6 +1,9 @@
+import Phaser from 'phaser';
 import MessageContainer from './ui/MessageContainer';
 import ProgressBar from './ui/ProgressBar';
 import Clock from './ui/Clock';
+import Settings from './Settings';
+import { HelpText } from './Text';
 
 export default class Hud {
   constructor(game) {
@@ -13,15 +16,61 @@ export default class Hud {
     this.clock.fixedToCamera(true);
     this.mc.fixedToCamera(true);
     this.pb.fixedToCamera(true);
+
+    this.isHelpDisplayed = false;
+    this.helpGraphics = this.game.add.graphics(100, 50);
+
+    const style = { font: `${Settings.Header1Size()}px ${Settings.FontStyle()}`, fill: Settings.FontColor() };
+    this.helpTextHeader = this.game.add.text(0, 70, 'Help Menu', style);
+    this.helpTextHeader.alpha = 0;
+    this.helpTextHeader.x = (this.game.width - this.helpTextHeader.width) / 2;
+
+    const style2 = { font: `${Settings.FontSize()}px ${Settings.FontStyle()}`, fill: Settings.FontColor() };
+    this.helpText = this.game.add.text(110, 120, HelpText.join('\n'), style2);
+    this.helpText.alpha = 0;
+
+    this.helpTextHeader.fixedToCamera = true;
+    this.helpText.fixedToCamera = true;
+
+    this.helpButton = this.game.add.button(700, 40 / 5, 'help', this.displayHelp, this, 0);
+    this.helpButton.fixedToCamera = true;
+
+    this.helpGraphics.lineStyle(4, 0x0000FF, 2);
+    this.helpGraphics.beginFill(0x000000);
+    this.helpGraphics.drawRect(0, 0, 600, 500);
+    this.helpGraphics.alpha = 0;
+    this.helpGraphics.fixedToCamera = true;
+
+    this.escapeKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
+  }
+
+  displayHelp() {
+    if (!this.isHelpDisplayed) {
+      this.helpGraphics.alpha = 1;
+      this.helpText.alpha = 1;
+      this.helpTextHeader.alpha = 1;
+    }
+    this.isHelpDisplayed = true;
+  }
+  hideHelp() {
+    this.helpGraphics.alpha = 0;
+    this.helpText.alpha = 0;
+    this.helpTextHeader.alpha = 0;
+    this.isHelpDisplayed = false;
   }
 
   createTopAnchor() {
-    this.graphics.beginFill(0x000000);
-    this.graphics.drawRect(0, 0, this.game.width, 40);
-    this.graphics.fixedToCamera = true;
+    const graphics2 = this.game.add.graphics(0, 0);
+    graphics2.alpha = 0.5;
+    graphics2.beginFill(0x000000);
+    graphics2.drawRect(0, 0, this.game.width, 40);
+    graphics2.fixedToCamera = true;
   }
 
   update() {
+    if (this.escapeKey.isDown && this.isHelpDisplayed) {
+      this.hideHelp();
+    }
     this.mc.update();
     this.pb.update();
     this.clock.update();
--- js/Text.js
+++ js/Text.js
@@ -6,7 +6,24 @@ export const IntroductionText = [
   'Me: Alright and how did we end up here...?',
   'Siri: Really do I have to answer all your questions? Just look at your teleporter it’s in pieces. I’m assuming based on the wreckage that it somehow malfunctioned. ',
   'Me: It doesn’t look busted up to me',
-  'Siri: That’s what happens when two developers and 0 designers work on a game.',
+  'Siri: That’s what happens when two developers and 0 designers work on a game. They didn\'t even take the time to render me!!',
+];
+
+export const HelpText = [
+  '- Use the arrow keys to move yourself',
+  '- Use the Enter key to skip to the next text',
+  '- Use the Spacebar to pick up items',
+  '- Use ESC to close this window',
+  '- Use ALT + F4 for awesome cheatcodes!',
+  '',
+  'also remember to pretend this is the best game ever*',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '*Probably isn\'t even close, but still 48hrs!',
 ];
 
 export const TextItem1 = () => 'test1';
