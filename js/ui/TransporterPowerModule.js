600ef76b1ce468bd90220feab45842f01599ea60
Edouard Shaar <Zarthia@gmail.com>
2018-01-28 13:38:05 -0500

    Removed the bag slots from transporter power module

--- js/ui/TransporterPowerModule.js
+++ js/ui/TransporterPowerModule.js
@@ -8,52 +8,24 @@ export default class TransporterPowerModule extends BaseDrawableObject {
     this.xSlots = 2;
     this.ySlots = 4;
 
-    const bagSlotsWidth = bag.xSlots * 42;
     const transporterSlotsWidth = this.xSlots * 42;
     const arrowWidth = 50;
-    this.width = bagSlotsWidth + arrowWidth + transporterSlotsWidth;
+    this.width = bag.width + arrowWidth + transporterSlotsWidth;
 
     const headerHeight = 20;
-    const bagSlotsHeight = bag.ySlots * 42;
-    const transporterSlotsHeight = this.ySlots * 42;
-    this.height = Math.max(bagSlotsHeight, transporterSlotsHeight) + headerHeight;
+    const transporterHeight = (this.ySlots * 42) + headerHeight;
+    this.height = Math.max(bag.height, transporterHeight);
 
     const containerX = (this.game.camera.width / 2) - (this.width / 2);
     const containerY = (this.game.camera.height / 2) - (this.height / 2);
 
-    // Draw Container
-    this.container = this.game.add.graphics(containerX, containerY);
-    this.container.alpha = 0.5;
-    this.container.lineStyle(2, 0x0000FF);
-    this.container.beginFill(0x000000);
-    this.container.drawRect(0, 0, this.width, this.height);
-
-    // Draw Title
-    this.textHeader = this.game.add.graphics(containerX, containerY);
-    this.textHeader.alpha = 0.8;
-    this.textHeader.lineStyle(2, 0x0000FF);
-    this.textHeader.beginFill(0x000000);
-    this.container.drawRect(0, 0, this.width, headerHeight);
-
-    const style = { font: `${Settings.Header4Size()}px ${Settings.FontStyle()}`, fill: Settings.FontColor() };
-    this.bagText = this.game.add.text(containerX + 5, containerY, 'Bag', style);
-    this.powerText = this.game.add.text(containerX + 130, containerY, 'Transporter', style);
-
-    // Draw Bag Slots
-    this.bagSlots = this.game.add.graphics(containerX, containerY + headerHeight);
-    this.bagSlots.alpha = 1;
-    this.bagSlots.lineStyle(1, 0XFFFFFF, 1);
-    for (let i = 0; i < bag.xSlots; i += 1) {
-      for (let k = 0; k < bag.ySlots; k += 1) {
-        this.bagSlots.drawRect((i * 40) + 4, (k * 40) + 4, 34, 34);
-      }
-    }
-
     // Draw Arrow
     this.arrow = this.game.add.graphics(
-      containerX + bagSlotsWidth + 2,
-      containerY + headerHeight + 20
+      containerX + bag.width + 2,
+      containerY + (this.height / 2) + (5 / 2)
     );
+    this.arrow.alpha = 0.8;
+    this.arrow.beginFill(0x000000);
     this.arrow.lineStyle(2, 0XFFFFFF);
     this.arrow.drawPolygon([
       { x: 0, y: 0 },
@@ -65,11 +37,31 @@ export default class TransporterPowerModule extends BaseDrawableObject {
       { x: 0, y: 10 },
       { x: 0, y: 0 }
     ]);
+    this.arrow.endFill();
+
+    const transporterX = bag.width + containerX + arrowWidth;
+
+    // Draw Container
+    this.container = this.game.add.graphics(transporterX, containerY);
+    this.container.alpha = 0.5;
+    this.container.lineStyle(2, 0x0000FF);
+    this.container.beginFill(0x000000);
+    this.container.drawRect(0, 0, transporterSlotsWidth + 6, this.height);
+
+    // Draw Title
+    this.textHeader = this.game.add.graphics(transporterX, containerY);
+    this.textHeader.alpha = 0.8;
+    this.textHeader.lineStyle(2, 0x0000FF);
+    this.textHeader.beginFill(0x000000);
+    this.container.drawRect(0, 0, transporterSlotsWidth + 6, headerHeight);
+
+    const style = { font: `${Settings.Header4Size()}px ${Settings.FontStyle()}`, fill: Settings.FontColor() };
+    this.powerText = this.game.add.text(transporterX + 4, containerY, 'Transporter', style);
 
     // Draw Transorter slots
     this.transporterSlots = this.game.add.graphics(
-      containerX + bagSlotsWidth + arrowWidth,
-      containerY + headerHeight
+      transporterX + 4,
+      containerY + headerHeight + 4
     );
     this.transporterSlots.alpha = 1;
     this.transporterSlots.lineStyle(1, 0XFFFFFF, 1);
@@ -83,9 +75,7 @@ export default class TransporterPowerModule extends BaseDrawableObject {
   fixedToCamera(fixedToCamera) {
     this.container.fixedToCamera = fixedToCamera;
     this.textHeader.fixedToCamera = fixedToCamera;
-    this.bagText.fixedToCamera = fixedToCamera;
     this.powerText.fixedToCamera = fixedToCamera;
-    this.bagSlots.fixedToCamera = fixedToCamera;
     this.arrow.fixedToCamera = fixedToCamera;
     this.transporterSlots.fixedToCamera = fixedToCamera;
   }
